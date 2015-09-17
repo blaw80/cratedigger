4b3906e509f8951a7f569f079528eb5b65213a8f
blaw80 <brianjohnlawlor@gmail.com>
2015-09-17 16:04:49 -0400

    added CORS to response headers

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ initPassport(passport);
 
 
 var routes = require('./routes/index')(passport);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', routes);
 
 app.use('/play', play);
